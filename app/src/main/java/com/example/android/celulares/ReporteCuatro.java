6a97245e622594bde7185cf5b273912cf7ecef2b
Armando <arjulio4@hotmail.com>
2017-10-07 02:41:00 -0500

    reportes terminados

---
+++ app/src/main/java/com/example/android/celulares/ReporteCuatro.java
@@ -0,0 +1,13 @@
+package com.example.android.celulares;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReporteCuatro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reporte_cuatro);
+    }
+}
